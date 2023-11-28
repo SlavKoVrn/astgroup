2f7b5bddeac84583195962f2fe9bc142f865f1d9
SlavKoVrn <slavko.chita@gmail.com>
2023-11-28 16:50:50 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u0320006_astgroup',
+    'username' => 'root',
+    'password' => '3306',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
